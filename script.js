85c5aed38fd3f406705276735cd92ad7ff53f7da
WoraponK <worapon.klabsri@gmail.com>
2023-07-06 17:24:35 +0700

    create sidepanel structure

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", () => {
     let header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 50)
 })
+
+
+
+let menuBtn = document.querySelector(".menu-btn");
+let closeBtn = document.querySelector(".close-btn");
+let menuExpand = document.querySelector(".menu-expand");
+
+menuBtn.addEventListener("click", () => {
+    menuExpand.classList.add("active");
+})
+closeBtn.addEventListener("click", () => {
+    menuExpand.classList.remove("active");
+})
